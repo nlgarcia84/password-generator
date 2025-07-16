3f91907cd4f684e36c32974a308cf27f6efc96a4
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2025-07-17 01:23:50 +0200

    functions done

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,30 @@
 import '../css/styles.css';
 
+const displayTextElement = document.getElementById('displayText');
+const lengthValueElement = document.getElementById('lengthValue');
+const inputRangeElement = document.getElementById('range');
 const formElement = document.getElementById('form');
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  console.log('Enviado');
+const allowedCharacters =
+  'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890+-.,!"·$%&/()=?{}';
+
+const setPasswordLength = () => {
+  lengthValueElement.textContent = inputRangeElement.value;
 };
 
-formElement.addEventListener('submit', handleSubmit);
+const generatePassword = () => {
+  let password = [];
+  for (let i = 0; i < inputRangeElement.value; i++) {
+    const randomNumber = Math.floor(Math.random() * allowedCharacters.length);
+    const randomCharacter = allowedCharacters.charAt(randomNumber);
+    password.push(randomCharacter);
+  }
+  const passwordString = password.join('');
+  displayTextElement.textContent = passwordString;
+};
+
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+  setPasswordLength();
+  generatePassword();
+});
