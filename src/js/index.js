b873b39e3d3607f29041fc711e58616d50458780
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2025-07-16 18:53:20 +0200

    css done

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
 import '../css/styles.css';
+
+const formElement = document.getElementById('form');
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log('Enviado');
+};
+
+formElement.addEventListener('submit', handleSubmit);
