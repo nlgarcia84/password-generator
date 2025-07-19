f0140c5fcab8066b3325e4a9da332af274228375
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2025-07-19 20:49:26 +0200

    anadida logica de los checkboxes

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,17 @@ const displayTextElement = document.getElementById('displayText');
 const lengthValueElement = document.getElementById('lengthValue');
 const inputRangeElement = document.getElementById('range');
 const formElement = document.getElementById('form');
+const submitButtonElement = document.getElementById('submitButton');
+const checkUpperCaseElement = document.getElementById('checkUpperCase');
+const checkLowerCaseElement = document.getElementById('checkLowerCase');
+const checkNumbersElement = document.getElementById('checkNumbers');
+const checkSymbolsElement = document.getElementById('checkSymbols');
 
 const allowedCharacters =
   'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890+-.,!"·$%&/()=?{}';
+const allowedNumbers = '1234567890';
+const allowedSymbols = '+-.,!"·$%&/()=?{}';
+let password = '';
 
 let passwordLength = inputRangeElement.value;
 lengthValueElement.textContent = passwordLength;
@@ -16,24 +24,73 @@ const setPasswordLength = () => {
   lengthValueElement.textContent = passwordLength;
 };
 
-const printPassword = (password) => {
-  displayTextElement.textContent = password;
-};
-
 const generatePassword = () => {
-  let password = '';
-  for (let i = 0; i < passwordLength; i++) {
-    const randomNumber = Math.floor(Math.random() * allowedCharacters.length);
-    const randomCharacter = allowedCharacters.charAt(randomNumber);
-    password += randomCharacter;
+  password = '';
+  if (checkUpperCaseElement.checked || checkLowerCaseElement.checked) {
+    for (let i = 0; i < passwordLength; i++) {
+      const randomNumber = Math.floor(Math.random() * allowedCharacters.length);
+      const randomCharacter = allowedCharacters.charAt(randomNumber);
+      password += randomCharacter;
+    }
   }
-  printPassword(password);
+  if (checkNumbersElement.checked) {
+    for (let i = 0; i < passwordLength; i++) {
+      const randomNumber = Math.floor(Math.random() * allowedNumbers.length);
+      const randomCharacter = allowedNumbers.charAt(randomNumber);
+      password += randomCharacter;
+    }
+  }
+  if (checkSymbolsElement.checked) {
+    for (let i = 0; i < passwordLength; i++) {
+      const randomNumber = Math.floor(Math.random() * allowedSymbols.length);
+      const randomCharacter = allowedSymbols.charAt(randomNumber);
+      password += randomCharacter;
+    }
+  }
+  return password;
 };
 
-inputRangeElement.addEventListener('input', setPasswordLength);
+const printPassword = () => {
+  const password = generatePassword();
+  if (checkUpperCaseElement.checked) {
+    displayTextElement.textContent = password.toUpperCase();
+  }
+  if (checkLowerCaseElement.checked) {
+    displayTextElement.textContent = password.toLowerCase();
+  }
+  if (checkUpperCaseElement.checked && checkLowerCaseElement.checked) {
+    displayTextElement.textContent = password;
+  }
+  if (checkNumbersElement.checked) {
+    displayTextElement.textContent = password;
+  }
+  if (checkSymbolsElement.checked) {
+    displayTextElement.textContent = password;
+  }
+};
 
+const activateSubmitButton = (e) => {
+  if (
+    checkUpperCaseElement.checked ||
+    checkLowerCaseElement.checked ||
+    checkNumbersElement.checked ||
+    checkSymbolsElement.checked
+  ) {
+    submitButtonElement.style.backgroundColor = '#a221d4';
+    submitButtonElement.disabled = false;
+  } else {
+    submitButtonElement.style.backgroundColor = '#90899e';
+    submitButtonElement.disabled = true;
+  }
+};
+
+checkUpperCaseElement.addEventListener('change', activateSubmitButton);
+checkLowerCaseElement.addEventListener('change', activateSubmitButton);
+checkNumbersElement.addEventListener('change', activateSubmitButton);
+checkSymbolsElement.addEventListener('change', activateSubmitButton);
+inputRangeElement.addEventListener('input', setPasswordLength);
 formElement.addEventListener('submit', (e) => {
   e.preventDefault();
   setPasswordLength();
-  generatePassword();
+  printPassword();
 });
